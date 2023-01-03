2a9c757846a54d726d22676313b342de01184b56
Дамир <damir.obuhov@mail.ru>
2023-01-03 14:05:10 +0400

    web2

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
 public class Demo1Application {
+    @GetMapping("/")
+    public String start() {
+        return "Приложение запущено";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
-	}
-
+    @GetMapping("/info")
+    public String info() {
+        return """
+                Имя ученика: Обухов Дамир Андреевич,
+                Название проекта: Web-development_Part2,
+                Дата создания проекта: 03.01.2023 г.,
+                Описание проекта в свободной форме: проект создан для выполнения домашнего задания. Реализуется с помощью сборщика Maven и набора инструментов Framework. Язык - Java""";
+    }
 }
